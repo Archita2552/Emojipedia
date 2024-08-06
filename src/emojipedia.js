e7d6c73d3adafe0cd8d2da2dadba05c9855d5e5f
Archita Jain <122081789+Archita2552@users.noreply.github.com>
2024-08-06 22:51:57 +0530

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,52 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id:5,
+    emoji:"🥹️",
+    name:"Face Holding Back Tears",
+    meaning:"shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride. At the same time, anger or rage can also be the reason why tears can hardly be held back."
+  },
+  {
+    id:6,
+    emoji:"😍️",
+    name:"Smiling Face With Heart-Eyes",
+    meaning:"It conveys intense love and affection for a person, place or even an object. "
+  },
+  {
+    id:7,
+    emoji:"😶‍🌫️",
+    name:"Face in clouds",
+    meaning:"This is nebulous! The face is surrounded by fog or clouds. Those who have their heads in the clouds are either dreamy or do not want to admit reality. The view of something is blurred. There may also be a state of confusion or bliss."
+  }
+  
+
+];
+
+export default emojipedia;
